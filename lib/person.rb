30dd6561948f729ddd2eed11db259eecc24a904d
gustaveH <ghabiman@gmail.com>
2020-02-18 23:00:55 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    def talk 
+        puts "Hello World!"
+    end
+    def walk 
+        puts "The Person is walking"
+    end 
+end 
+
+gustave = Person.new
+gustave.talk
+gustave.walk 
